20f4ce8c3aad71d03f3aa59b007c27f6413b3862
Kailey Lampert <kailey.lampert@automattic.com>
2021-11-24 18:30:52 +0000

    reshuffle

--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 yes | cp -rf ~/mine/vip-go-sandbox/mu-plugins/00-sandbox-helper.php /var/www/wp-content/mu-plugins/.
 
-alias logs="tail -F /tmp/php-errors -F /chroot/tmp/php-errors"
-alias run-crons="wp site list --path=/chroot/var/www --field=url | xargs -I URL wp --path=/chroot/var/www cron event run --due-now --url=URL"
 
 git config --global user.email "kailey.lampert@automattic.com"
 git config --global user.name "Kailey Lampert"
 
 # self-update
 git -C ~/mine/vip-go-sandbox/ pull > /dev/null
-
-export WP_CLI_PACKAGES_DIR='/root/mine/wpcli-packages'
-mkdir -p $WP_CLI_PACKAGES_DIR
 
 #if [ ! -d /mine/vip-go-sandbox/tools/vip-cli/dist ]; then
 #	pushd /mine/vip-go-sandbox/tools/vip-cli
@@ -26,6 +21,9 @@ cp .vimrc ~/.vimrc
 cp .mybashrc ~/.mybashrc
 
 if [ ! $(cat ~/.bashrc | grep mybashrc) ]; then
+  echo 'source ~/.mybashrc' >> ~/.bashrc
+fi
+if [ ! $(cat ~/.bashrc | grep prompt) ]; then
   echo 'source ~/.prompt' >> ~/.bashrc
 fi
 
