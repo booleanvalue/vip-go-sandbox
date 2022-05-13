b47e324dbc8a3db59187dd1c40d70ab357bfe36f
Kailey Lampert <trepmal@gmail.com>
2022-05-13 16:14:24 -0700

    Update self-installer.sh

--- self-installer.sh
+++ self-installer.sh
@@ -4,10 +4,10 @@ pushd ~ > /dev/null
 
 if [[ ! -d ~/mine/ ]]; then
   mkdir mine && pushd mine > /dev/null
-  git clone git@github.com:trepmal/vip-go-sandbox.git && pushd vip-go-sandbox > /dev/null && bash install.sh && popd
+  git clone git@github.com:trepmal/vip-go-sandbox.git && pushd vip-go-sandbox > /dev/null && bash install.sh && popd > /dev/null
 elif [[ ! -d ~/mine/vip-go-sandbox ]]; then
   pushd mine > /dev/null
-  git clone git@github.com:trepmal/vip-go-sandbox.git && pushd vip-go-sandbox > /dev/null && bash install.sh && popd
+  git clone git@github.com:trepmal/vip-go-sandbox.git && pushd vip-go-sandbox > /dev/null && bash install.sh && popd > /dev/null
 else
   pushd mine/vip-go-sandbox > /dev/null && bash install.sh
 fi
