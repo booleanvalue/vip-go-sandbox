8d557af83d6aee07443a38bf782cec1b3cab8fb0
Kailey Lampert <trepmal@gmail.com>
2024-01-23 14:17:01 -0800

    use sandbox-customzied concat-utils

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+#
+# Install our stuff
+#
+
+## mu-plugins
 yes | cp -rf ~/mine/vip-go-sandbox/mu-plugins/00-sandbox-helper.php /var/www/wp-content/mu-plugins/.
 
+mv /var/www/wp-content/mu-plugins/http-concat/concat-utils.php /var/www/wp-content/mu-plugins/http-concat/concat-utils.php.original
+yes | cp -rf ~/mine/vip-go-sandbox/mu-plugins/concat-utils.php /var/www/wp-content/mu-plugins/http-concat/concat-utils.php
 
+##
 git config --global user.email "kailey.lampert@automattic.com"
 git config --global user.name "Kailey Lampert"
 git config --global pull.rebase false
 
 # self-update
 git -C ~/mine/vip-go-sandbox/ pull --rebase > /dev/null
-
-#if [ ! -d /mine/vip-go-sandbox/tools/vip-cli/dist ]; then
-#	pushd /mine/vip-go-sandbox/tools/vip-cli
-#	npm install
-#	npm run build
-#	popd
-#fi
 
 # don't check if files exist so they'll always get updated when this script runs
 cp .prompt ~/.prompt
