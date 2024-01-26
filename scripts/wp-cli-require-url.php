3d268feb27c76ceeb2ad1c29c400d3b48550dae1
Kailey Lampert <trepmal@gmail.com>
2024-01-25 16:48:48 -0800

    simplify commands allowed without --url

--- scripts/wp-cli-require-url.php
+++ scripts/wp-cli-require-url.php
@@ -4,18 +4,18 @@ $argv = array_slice( $GLOBALS['argv'], 1 );
 list( $args, $assoc_args, $runtime_config ) = $configurator->parse_args( $argv );
 WP_CLI::add_wp_hook( 'setup_theme', function() use( $args, $assoc_args, $runtime_config ) {
  
-    // allow all help commands
-    if ( 'help' === strtolower( $args[0] ) ) {
-        return;
-    }
-    // allow site list
+    // allowed without --url
     if (
-        'site' === strtolower( $args[0] ) &&
-        'list' === strtolower( $args[1] )
+        ( 'help' === strtolower( $args[0] ) ) ||
+        ( 'site' === strtolower( $args[0] ) &&  'list' === strtolower( $args[1] ) ) ||
+        ( 'network' === strtolower( $args[0] ) &&  'list' === strtolower( $args[1] ) ) ||
+        ( 'app' === strtolower( $args[0] ) ) ||
+        ( 'sandbox' === strtolower( $args[0] ) )
     ) {
         return;
     }
 
+    // default state
     $method = 'warning';
 
     // sometimes we just need a scratch pad, don't make shell too hard
