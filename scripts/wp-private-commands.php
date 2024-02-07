ed5c77ffc06b17c56951b6d98db1eb9f98af4279
Kailey Lampert <trepmal@gmail.com>
2024-02-07 09:29:52 -0800

    include private commands from their original location

--- scripts/wp-private-commands.php
+++ scripts/wp-private-commands.php
@@ -1,9 +1,14 @@
 <?php
 
 /**
- * commands excluded from this repo
+ * Load private commands
  * https://github.com/trepmal/vip-go-sandbox-priv/
+ *
+ * Some commands should not be publicly-accessible and are in the above
+ * optional repo, so we include those via this loop, so prevent breakage
+ * if not installed
+ *
  */
-foreach ( glob( __DIR__ . '/priv/*.php' ) as $command ) {
+foreach ( glob( '/root/commands/*.php' ) as $command ) {
 	require_once $command;
 }
