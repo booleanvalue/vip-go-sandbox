3834a4e77443e0cd1df846edd0271dd255bfd179
Kailey Lampert <trepmal@gmail.com>
2024-09-10 22:03:59 -0700

    fix whitespace, typo

--- mu-plugins/00-sandbox-helper.php
+++ mu-plugins/00-sandbox-helper.php
@@ -11,30 +11,30 @@ ini_set( 'error_log', '/tmp/php-errors' );
 
 // fancy var_dump
 if ( ! function_exists( 'vip_dump' ) ) {
-    function vip_dump( ...$var ) {
-        $old_setting = ini_get( 'html_errors' );
-        ini_set( 'html_errors', false );
-        ini_set( 'xdebug.cli_color', 2 );
-        ob_start();
-        foreach ( $var as $v ) {
-		var_dump( v );
+	function vip_dump( ...$var ) {
+		$old_setting = ini_get( 'html_errors' );
+		ini_set( 'html_errors', false );
+		ini_set( 'xdebug.cli_color', 2 );
+		ob_start();
+		foreach ( $var as $v ) {
+			var_dump( $v );
+		}
+		$out1 = ob_get_contents();
+		ob_end_clean();
+		error_log( $out1 );
+		ini_set( 'xdebug.cli_color', 1 );
+		ini_set( 'html_errors', $old_setting );
 	}
-        $out1 = ob_get_contents();
-        ob_end_clean();
-        error_log( $out1 );
-        ini_set( 'xdebug.cli_color', 1 );
-        ini_set( 'html_errors', $old_setting );
-    }
 }
 
 // show last error on error handler
-add_filter( 'wp_php_error_message', function( $m, $e ) { 
-    $m .= '<dl>';
-    foreach ( $e as $k => $v ) { 
-        $m .= "<dt>$k</dt><dd>$v</dd>";
-    }
-    $m .= '</dl>';
-    return $m; 
+add_filter( 'wp_php_error_message', function( $m, $e ) {
+	$m .= '<dl>';
+	foreach ( $e as $k => $v ) {
+		$m .= "<dt>$k</dt><dd>$v</dd>";
+	}
+	$m .= '</dl>';
+	return $m;
 }, 10, 2 );
 
 add_filter( 'x_redirect_by', function( $x_redirect_by, $status, $location ) {
@@ -46,5 +46,5 @@ add_filter( 'x_redirect_by', function( $x_redirect_by, $status, $location ) {
 }, 10, 3 );
 
 if ( file_exists( __DIR__ . '/sandbox-wp-debugger/sandbox-wp-debugger.php' ) ) {
-    require_once __DIR__ . '/sandbox-wp-debugger/sandbox-wp-debugger.php';
+	require_once __DIR__ . '/sandbox-wp-debugger/sandbox-wp-debugger.php';
 }
